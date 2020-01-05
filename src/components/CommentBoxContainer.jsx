34a3e347652a4a47a871141723d8de79977b269e
PG066149 <Pavan.GM@cerner.com>
2020-01-05 15:45:58 +0530

    added a view to show comment box

--- src/components/CommentBoxContainer.jsx
+++ src/components/CommentBoxContainer.jsx
@@ -1,14 +1,45 @@
 import React from 'react';
+import CommentBoxView from './CommentBoxView';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as CommentBoxActions from '../actions/CommentBoxActions';
+import commentBoxReducer from '../reducers/CommentBoxReducer';
+import CommentsView from './CommentsView';
 
 class CommentBoxContainer extends React.Component {
-    render() {
-        return (
 
-            <div>
-                <h1>Hello pavan</h1>
-            </div>
+saveComment(comment){
+    this.props.commentBoxTypeActions.saveCommentAction(comment);
+}
+
+    render() {
+        const { commentsList } = this.props;
+        return (
+            <>
+            <CommentBoxView
+                saveComment={this.saveComment.bind(this)}
+                
+            />
+            <CommentsView
+            commentsList={commentsList}
+            />
+            </>
         );
     }
 }
 
-export default CommentBoxContainer;
+const mapStateToProps = state => ({
+    commentsList: state.commentBoxReducerState.commentsList,
+});
+
+function mapDispatchToProps(dispatch) {
+    return {
+        commentBoxTypeActions: bindActionCreators(CommentBoxActions, dispatch),
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CommentBoxContainer);
+
+const commentBoxReducers = Object.assign({}, { commentBoxReducerState: commentBoxReducer });
+
+export { commentBoxReducers };
--- src/startup/CommentBoxApplication.jsx
+++ src/startup/CommentBoxApplication.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import CommentBoxContainer from '../components/CommentBoxContainer';
+import createSagaMiddleware from 'redux-saga';
+import CommentBoxContainer, { commentBoxReducers } from '../components/CommentBoxContainer';
+import CommentBoxSaga from '../sagas/CommentBoxSaga';
+import '../App.css';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  combineReducers({...commentBoxReducers}),
+  compose(applyMiddleware(sagaMiddleware)),
+);
+
+// CommentBoxSaga.map(saga => (sagaMiddleware.run(saga)));
 
 class CommentBoxApplication extends React.Component {
+
   render() {
     return (
-      // <Provider>
-        <CommentBoxContainer />
-      // </Provider>
+      <Provider store={store}>
+        <CommentBoxContainer className='App'/>
+      </Provider>
     );
 
   }
